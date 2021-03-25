03cdc481e5cb50474689f916067254bb40801ba0
Matey Nikolov <mateynnikolov@gmail.com>
2021-03-25 21:11:46 +0100

    test1

---
+++ testfile2x.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello"
+read sentence
+echo "How are you"
+read sentence
+echo "What is your name"
+read name
+echo "Nice to meet you, $name!"
+read sentence
+echo "My name is Echo"
